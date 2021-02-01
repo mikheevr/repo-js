8172528237c281c38a8905f1cc735cceafd86e79
Ruslan <mikheevr19@gmail.com>
2021-02-01 17:42:14 +0600

    Задание после 1 урока

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict"
+var Tc = 25
+var Tf = (9 * Tc) / 5 + 32
+alert("Tf=" + Tf)
+var name = "Василий"
+var admin = name
+alert(admin)
+//Js выражение 1000 + "108" = "1000108" (если я все верно понял на уроке)
